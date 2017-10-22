25c863bc84c3c771dc6b6b18d19208ac5d8e5719
hamed <zitoun.hamed@gmail.com>
2017-10-22 12:26:05 +0200

    code refactor for parser impl class

--- src/main/java/com/hzitoun/xtt/parsers/impl/MowerParserIpml.java
+++ src/main/java/com/hzitoun/xtt/parsers/impl/MowerParserIpml.java
@@ -41,45 +41,11 @@ public class MowerParserIpml implements IMowerParser {
 						throw new MowerAppException("Your file is not well formated at line " + (index + 1));
 					}
 					if (index == 0) {
-						final String[] resolution = line.split(" ");
-						if (resolution.length == 2 && Utils.isNumeric(resolution[0])
-								&& Utils.isNumeric(resolution[1])) {
-							surface = new Surface(Integer.parseInt(resolution[0]), Integer.parseInt(resolution[1]));
-						} else {
-							throw new MowerAppException("Your file is not well formated at line " + (index + 1));
-						}
+						surface = parseSurface(index, line);
 					} else if (index > 0 && index % 2 == 1) {
-						final String[] mowerCaracteristics = line.split(" ");
-						if (mowerCaracteristics.length == 3 && Utils.isNumeric(mowerCaracteristics[0])
-								&& Utils.isNumeric(mowerCaracteristics[1])
-								&& Utils.isAlpha(mowerCaracteristics[2])
-								&& EnumDirection.isDirectionValid(mowerCaracteristics[2])) {
-							final EnumDirection direction = EnumDirection.valueOf(mowerCaracteristics[2].toUpperCase());
-							final Position position = new Position(Integer.parseInt(mowerCaracteristics[0]),
-									Integer.parseInt(mowerCaracteristics[1]));
-							mower = new Mower(position, direction);
-							if (index + 1 >= lines.size()) {
-								throw new MowerAppException("Missing commands for the last mower ");
-							}
-						} else {
-							throw new MowerAppException("Your file is not well formated at line " + (index + 1));
-						}
+						mower = parseMower(lines, index, line);
 					} else if (index > 0 && index % 2 == 0) {
-						final List<Command> commands = new ArrayList<>();
-						for (final char commandAsChar : line.toCharArray()) {
-							if (!EnumAction.isActionValid(commandAsChar)) {
-								throw new MowerAppException("Your file is not well formated at line " + (index + 1));
-							}
-							final EnumAction action = EnumAction.valueOf(String.valueOf(commandAsChar));
-							if (EnumAction.A.equals(action)) {
-								commands.add(new MoveForwardCommand(mower));
-							} else if (EnumAction.G.equals(action)) {
-								commands.add(new TurnLeftCommand(mower));
-							} else if (EnumAction.D.equals(action)) {
-								commands.add(new TurnRightCommand(mower));
-							}
-						}
-						mowers.put(mower, commands);
+						parseMowerCommands(mower, mowers, index, line);
 					}
 				}
 				final MowerAppCharacteristics app = new MowerAppCharacteristics(surface, mowers);
@@ -90,11 +56,57 @@ public class MowerParserIpml implements IMowerParser {
 		} catch (final IOException e) {
 			e.printStackTrace();
 			throw new MowerAppException("An exception has occurred when reading the file");
-			
+
 		}
 	}
 
-	private List<String> readLinesFromFile(Path inputFile) throws IOException {
+	private final void parseMowerCommands(final Mower mower, final Map<Mower, List<Command>> mowers, int index,
+			final String line) throws MowerAppException {
+		final List<Command> commands = new ArrayList<>();
+		for (final char commandAsChar : line.toCharArray()) {
+			if (!EnumAction.isActionValid(commandAsChar)) {
+				throw new MowerAppException("Your file is not well formated at line " + (index + 1));
+			}
+			final EnumAction action = EnumAction.valueOf(String.valueOf(commandAsChar));
+			if (EnumAction.A.equals(action)) {
+				commands.add(new MoveForwardCommand(mower));
+			} else if (EnumAction.G.equals(action)) {
+				commands.add(new TurnLeftCommand(mower));
+			} else if (EnumAction.D.equals(action)) {
+				commands.add(new TurnRightCommand(mower));
+			}
+		}
+		mowers.put(mower, commands);
+	}
+
+	private final Mower parseMower(final List<String> lines, final int index, final String line)
+			throws MowerAppException {
+		final String[] mowerCaracteristics = line.split(" ");
+		if (mowerCaracteristics.length == 3 && Utils.isNumeric(mowerCaracteristics[0])
+				&& Utils.isNumeric(mowerCaracteristics[1]) && Utils.isAlpha(mowerCaracteristics[2])
+				&& EnumDirection.isDirectionValid(mowerCaracteristics[2])) {
+			final EnumDirection direction = EnumDirection.valueOf(mowerCaracteristics[2].toUpperCase());
+			final Position position = new Position(Integer.parseInt(mowerCaracteristics[0]),
+					Integer.parseInt(mowerCaracteristics[1]));
+			if (index + 1 >= lines.size()) {
+				throw new MowerAppException("Missing commands for the last mower ");
+			}
+			return new Mower(position, direction);
+		} else {
+			throw new MowerAppException("Your file is not well formated at line " + (index + 1));
+		}
+	}
+
+	private final Surface parseSurface(final int index, final String line) throws MowerAppException {
+		final String[] resolution = line.split(" ");
+		if (resolution.length == 2 && Utils.isNumeric(resolution[0]) && Utils.isNumeric(resolution[1])) {
+			return new Surface(Integer.parseInt(resolution[0]), Integer.parseInt(resolution[1]));
+		} else {
+			throw new MowerAppException("Your file is not well formated at line " + (index + 1));
+		}
+	}
+
+	private final List<String> readLinesFromFile(final Path inputFile) throws IOException {
 		try (final Stream<String> stream = Files.lines(inputFile)) {
 			return stream.collect(Collectors.toList());
 		}
